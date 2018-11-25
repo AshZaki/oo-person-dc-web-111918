f4caf8abb62df677ca6beddd7b66fd66ee7b9831
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 02:18:16 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
-# your code goes here
+class Person
+  attr_accessor :bank_account,:happiness, :hygiene
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+  
+  def happiness=(new_happiness)
+    @happiness = new_happiness
+    if new_happiness > 10
+      @happiness = 10
+    elsif new_happiness < 0
+      @happiness = 0
+    end
+  end
+  
+  def take_bath
+      self.hygiene += 4
+      return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  
+  def 
+  end
+end
+
+  
